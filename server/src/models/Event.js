aa498ae7e60277c2255a166c70110e5c7c4b12b2
mshkolnik22 <75180384+mshkolnik22@users.noreply.github.com>
2021-02-10 17:39:58 -0500

    created a seeder, worked with events table and jsonSchema, made date and time Strings

--- server/src/models/Event.js
+++ server/src/models/Event.js
@@ -4,6 +4,25 @@ class Event extends Model {
   static get tableName() {
     return "events"
   }
+
+  static get jsonSchema() {
+    return {
+      type: "object",
+      required: ["title", "hostedBy", "hostEmail", "date"],
+      properties: {
+        title: { type: "string" },
+        typeOfEvent: { type: "string" },
+        description: { type: "string" },
+        hostedBy: { type: "string" },
+        hostEmail: { type: "string" },
+        linkURL: { type: "string" },
+        location: { type: "string" },
+        date: { type: "string", format: "date" },
+        time: { type: "string", format: "time" },
+        reminder: { type: ["string", "integer"] },
+      },
+    }
+  }
 }
 
 module.exports = Event
